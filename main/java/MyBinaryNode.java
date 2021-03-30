125e8e33765f2a9d818ec95253101c8c033ddcfd
saitarun135 <saitarun135@gm.com>
2021-03-30 17:56:11 +0530

    [Added]BST creation

--- main/java/MyBinaryNode.java
+++ main/java/MyBinaryNode.java
@@ -1,6 +1,13 @@
 
-public class MyBinaryNode {
-	public static void main(String args[]) {
-		System.out.println("Welcome to the Binary Tree concept");
-	}
+public class MyBinaryNode<T extends Comparable> {
+	T key;
+	MyBinaryNode<T> left;
+	MyBinaryNode<T> right;
+	
+	public MyBinaryNode(T key) {
+		this.key=key;
+		this.left=null;
+		this.right=null;
+		}
+	
 }
---
+++ main/java/MyBinaryTree.java
@@ -0,0 +1,36 @@
+
+public class MyBinaryTree<T extends Comparable> {
+	private MyBinaryNode<T> root;
+	
+	public void add(T key) {
+		this.root=this.addRecursively(root, key);
+	}
+	
+	private MyBinaryNode<T> addRecursively(MyBinaryNode<T> current,T key) {
+		if(current==null) return new MyBinaryNode<>(key);
+		
+		int compareResult=key.compareTo(current.key);
+		
+		if(compareResult==0) return current;
+		
+		if(compareResult < 0) {
+							current.left=(MyBinaryNode<T>) addRecursively((MyBinaryNode<T>) current.left,key);
+			
+		}else 
+		{
+							current.right=(MyBinaryNode<T>) addRecursively((MyBinaryNode<T>) current.right,key);
+			
+		}
+		return current;
+	}
+
+	public int getSize() {
+		return this.getSizeRecursive(root);
+	}
+	
+	private int getSizeRecursive(MyBinaryNode<T> current) {
+	
+		return current== null ? 0:1 +this.getSizeRecursive((MyBinaryNode<T>) current.left)
+									+this.getSizeRecursive((MyBinaryNode<T>) current.right);					
+	}
+}
